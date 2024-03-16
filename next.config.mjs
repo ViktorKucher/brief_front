9db99a2e682443243055fc6cc690a05edaabe698
Kucher Viktor <viktorkucer49@gmail.com>
2024-03-16 22:15:37 +0200

    d

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-      NEXT_PUBLIC_SERVER_API:'https://brief-ten.vercel.app/'
+      NEXT_PUBLIC_SERVER_API:'https://brief-backend.vercel.app'
       }
 };
 
