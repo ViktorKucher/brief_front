1354a221093f251133acf821a53250557ed242a8
Kucher Viktor <viktorkucer49@gmail.com>
2024-03-17 08:52:31 +0200

    config changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env:{
-      NEXT_PUBLIC_SERVER_API:'https://brief-backend.vercel.app'
-      }
+  reactStrictMode: true,
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+  env: {
+    NEXT_PUBLIC_SERVER_API: "https://brief-backend.vercel.app",
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "/api/:path*",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://brief-front.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE",
+          }, // Adjust methods as needed
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          }, // Adjust headers as needed
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
